5e05b34f96c1b9247519bc72e310e45f253444e8
Carlos Barrios <52840766+CarlosBarrios045@users.noreply.github.com>
2019-11-08 23:54:27 -0400

    Corregido error en el compose de Redux

--- src/store/store.js
+++ src/store/store.js
@@ -1,19 +1,15 @@
-import { compose, createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 
 // Reducer
 import rootReducer from './rootReducer';
-
-// Redux-thunk
-const middleware = [thunk];
 
 // InitialState
 const initialState = {}
 
 // Store
 const store = createStore(rootReducer, initialState, 
-  compose(applyMiddleware(...middleware), 
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-) );
+  applyMiddleware(thunk)
+);
 
 export default store;
